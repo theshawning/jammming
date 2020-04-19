857f4b8594059c163fd1faa889971e0702f35e7e
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-19 11:18:52 +0000

    initial commit, cleaned boiler setup from create-react-app and updated app.js

---
+++ src/components/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Ja<span className='highlight'>mmm</span>ing</h1>
+      <div className="App">
+        [//searchbar component]
+        <div className='App-playlist'>
+          [//search results component]
+          [//playlist component]
+        </div>
+      </div>    
+    </div>
+  );
+}
+
+export default App;
