f185024e5f3f8630b0d293d16b546195a4dfaf6b
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-20 14:20:27 +0000

    added state to searchBar component and allowed user to edit playlist name

--- src/components/playlist/playlist.component.jsx
+++ src/components/playlist/playlist.component.jsx
@@ -4,15 +4,29 @@ import './playlist.style.scss';
 import TrackList from '../tracklist/tracklist.component';
 
 class PlayList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.props.onNameChange(e.target.value);
+  }
+
   render() {
     return (
       <div className='Playlist'>
-        <input defaultValue={'New Playlist'} />
+        <input 
+          defaultValue={'New Playlist'} 
+          onChange={this.handleNameChange} />
         <TrackList 
-          tracks={this.props.playlistTracks}
-          onRemove={this.props.onRemove} 
+          tracks={this.props.playlistTracks} 
+          onRemove={this.props.onRemove}  
           isRemoval={true} />
-        <button className='Playlist-save-btn'>SAVE TO SPOTIFY</button>
+        <button 
+          className='Playlist-save-btn' 
+          onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     )
   }
