c21736bdc757553f1dbe35f2dca65c95c3919125
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-20 15:45:36 +0000

    finished intial design of Spotify component allowing user to save playlist, but still broken. Getting error objects are not valid as a react child

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,17 +10,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { searchResults: [
-        {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
-        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-      ],
-      playlistName: 'Favorite Songs',
-      playlistTracks: [
-        {name: 'favsong', artist: 'favartist', album: 'favalbum', id: 4},
-        {name: 'nextfav', artist: 'secondBest', album: 'whatev', id: 5},
-        {name: 'thirdbest', artist: 'whodat', album: 'debutStar', id: 6}
-      ]
+    this.state = { searchResults: [],
+      playlistName: 'My Playlist',
+      playlistTracks: []
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -53,12 +45,18 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
     Spotify.search(term).then(searchResults => {
       this.setState({ searchResults: searchResults })
-    })
+    });
   }
 
   render() {
--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,4 +1,4 @@
-import clientId from './clientId';
+import { clientId } from './clientId';
 
 const redirectUri = 'http://localhost:3000'
 let accessToken;
@@ -45,8 +45,36 @@ const Spotify = {
         uri: track.uri
       }));
     })
-  }
+  },
 
+  savePlayList(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+      {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({ name: name })
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({ uris: trackUris })
+        })
+      })
+    })
+  }
 };
 
 export default Spotify;
